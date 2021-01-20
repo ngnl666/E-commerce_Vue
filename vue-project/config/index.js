9d30329d556d3bafe70e9c2fd0f2081645973975
Arron <e3e38888@gmail.com>
2021-01-20 16:32:12 +0800

    < 2021/1/20 >

--- vue-project/config/index.js
+++ vue-project/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/E-commerce_Vue/',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
